6e7d5c0c304fe9d773b305960fcfe6963210e9e3
RichardDufour <richarddufour@laposte.net>
2022-02-02 22:16:30 +0100

    V1.4

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './header.css'
 
 const Header = () => {
-  return <div className="gpt3__header section__padding" id="home">
+  return <div className="gpt3__header section__padding" id="generation">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
         <p>
