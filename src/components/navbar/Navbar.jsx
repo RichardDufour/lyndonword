d82f647a16007ca5cab7dc0ff80a015af8607d03
RichardDufour <richarddufour@laposte.net>
2022-02-02 21:21:37 +0100

    V1.2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg'
 import './navbar.css'
 
 // BEM -> Block Element Modifier
@@ -20,7 +19,6 @@ const Navbar = () => {
   return(<div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo"> 
-          <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
           <Menu />
