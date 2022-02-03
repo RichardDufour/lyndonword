59ebd334bec32735ac75335582f203ae77ee8df3
RichardDufour <richarddufour@laposte.net>
2022-02-03 21:47:11 +0100

    MathTest

--- src/components/proposition/Proposition.jsx
+++ src/components/proposition/Proposition.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './proposition.css'
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 const Proposition = ({ typeOfProposition, title, desc }) => {
   return <MathJaxContext config={config}>
