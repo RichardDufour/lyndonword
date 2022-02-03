476e82bd1a501665b83470ff549fdd24fead175f
RichardDufour <richarddufour@laposte.net>
2022-02-03 20:46:49 +0100

    MathsFormula

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import { Navbar } from './components';
 import { Header } from './containers';
 import React from 'react'
 import ReactDOM from 'react-dom'
-import FractionDisplay from './maths/Formula';
+import { Formula } from './maths';
+import { Fraction, toTex } from 'algebra.js';
 
 function App() {
-  FractionDisplay()
+
+  const a = new Fraction(1, 5);
+  const b = new Fraction(2, 7);
+  const answer = a.multiply(b);
+
   return (
     <div className="App gradient__bg">
       <div className="gradient__bg">
         <Navbar />
         <Header />
+        <Formula tex={`${toTex(a)} × ${toTex(b)} = ${toTex(answer)}`} />
       </div>
     </div>
   );
--- src/maths/Formula.jsx
+++ src/maths/Formula.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Fraction, toTex } from 'algebra.js';
 import { Node, Context } from 'react-mathjax2';
 
-function Formula(props) {
+const Formula = ({ props }) => {
   return (
     <Context input="tex">
       <Node inline>{props.tex}</Node>
     </Context>
   );
-}
+};
 
+export default Formula;
+
+/*
 export default function FractionDisplay() {
     const a = new Fraction(1, 5);
     const b = new Fraction(2, 7);
@@ -22,4 +25,4 @@ export default function FractionDisplay() {
         {question}
       </div>
     );
-  }
+  } */
