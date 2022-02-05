a54d4bccf986c4766ca7656155204b6ce8b3f7b0
RichardDufour <richarddufour@laposte.net>
2022-02-05 18:07:33 +0100

    Vtest1.1

--- src/components/proposition/Proposition.jsx
+++ src/components/proposition/Proposition.jsx
@@ -3,11 +3,13 @@ import './proposition.css'
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 const Proposition = ({ typeOfProposition, title, desc, config }) => {
-  return <MathJaxContext config={config}>
+  return <div className="lyndonwords__proposition">
+  <MathJaxContext config={config}>
             <h1 className="lyndonwords__proposition-type"> {typeOfProposition} </h1>
             <h2 className="lyndonwords__proposition-title">{title}</h2>
             <MathJax>{desc}</MathJax>
-        </MathJaxContext>;
+        </MathJaxContext>
+        </div> ;
 };
 
 export default Proposition;
